4f20f960c105b7e6379ee883cc11e9b4d89a923b
suryamudti <suryamudti0128@gmail.com>
2020-08-04 06:39:05 +0700

    add form

--- src/webapp/Phrases.kt
+++ src/webapp/Phrases.kt
@@ -1,14 +1,18 @@
 package com.smile.webapp
 
+import com.smile.model.EmojiPhrase
 import com.smile.model.User
 import com.smile.repository.Repository
 import io.ktor.application.call
 import io.ktor.auth.authenticate
 import io.ktor.auth.authentication
 import io.ktor.freemarker.FreeMarkerContent
+import io.ktor.request.receiveParameters
 import io.ktor.response.respond
+import io.ktor.response.respondRedirect
 import io.ktor.routing.Route
 import io.ktor.routing.get
+import io.ktor.routing.post
 
 const val PHRASES = "/phrases"
 
@@ -19,6 +23,13 @@ fun Route.phrases(db: Repository) {
             val phrases = db.phrases()
             call.respond(FreeMarkerContent("phrases.ftl", mapOf("phrases" to phrases,
                 "displayName" to user.displayName)))
+        }
+        post(PHRASES) {
+            val params = call.receiveParameters()
+            val emoji = params["emoji"] ?: throw IllegalArgumentException("Missing parameter: emoji")
+            val phrase = params["phrase"] ?: throw IllegalArgumentException("Missing parameter: phrase")
+            db.add(EmojiPhrase(emoji, phrase))
+            call.respondRedirect(PHRASES)
         }
     }
 }
