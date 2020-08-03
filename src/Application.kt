fe1a2ff2e52ee5939f843831f17f28d0c7111406
suryamudti <suryamudti0128@gmail.com>
2020-08-03 21:44:02 +0700

    initial commit

---
+++ src/Application.kt
@@ -0,0 +1,16 @@
+package com.smile
+
+import com.smile.webapp.home
+import io.ktor.application.Application
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        home()
+    }
+}
+
---
+++ src/webapp/Home.kt
@@ -0,0 +1,14 @@
+package com.smile.webapp
+
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.Route
+import io.ktor.routing.get
+
+const val HOME = "/"
+
+fun Route.home() {
+    get(HOME) {
+        call.respondText("hello world !")
+    }
+}
